6a4c063ad1446f8270ebf871989f27f779b1f1c8
irozenf <irozenf@ecitele.com>
2018-07-05 16:07:31 +0300

    build

--- template-client/src/main/java/com/company/template/client/web/dtos/catalogue/CatalogueResponse.java
+++ template-client/src/main/java/com/company/template/client/web/dtos/catalogue/CatalogueResponse.java
@@ -1,8 +1,25 @@
+/**
+ * Copyright (C) 2018 user name (user@email.com) the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.company.template.client.web.dtos.catalogue;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import io.swagger.annotations.ApiModel;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -10,7 +27,8 @@ import lombok.RequiredArgsConstructor;
  */
 @ApiModel("Catalogue Response")
 @Getter
-@RequiredArgsConstructor(staticName = "of", onConstructor = @__({@JsonCreator}))
+@NoArgsConstructor
+@AllArgsConstructor(staticName = "of")
 public class CatalogueResponse {
-    private final Long productId;
+    private Long productId;
 }
